789f84eb92f8ec3fd6a6632297e408a77f9a9987
Lukas Truemper <lukas.truemper@outlook.de>
2025-05-22 15:11:38 +0200

    adds instrumentation with additional metadata

--- src/codegen/instrumentation/outermost_loops_instrumentation.cpp
+++ src/codegen/instrumentation/outermost_loops_instrumentation.cpp
@@ -24,13 +24,32 @@ void OutermostLoopsInstrumentation::begin_instrumentation(const structured_contr
 }
 
 void OutermostLoopsInstrumentation::end_instrumentation(const structured_control_flow::ControlFlowNode& node, PrettyPrinter& stream) const {
-    stream << "__daisy_instrument_exit(";
-    stream << "\"" << schedule_.sdfg().name() << "_" << node.name() << "\", ";
+    std::string region_name = schedule_.sdfg().name() + "_" + node.name();
+
+    bool has_metadata = schedule_.sdfg().metadata().find("source_file") != schedule_.sdfg().metadata().end() && schedule_.sdfg().metadata().find("features_path") != schedule_.sdfg().metadata().end();
+
+    if (has_metadata) {
+        stream << "__daisy_instrument_exit_with_metadata(";
+    } else {
+        stream << "__daisy_instrument_exit(";
+    }
+
+    stream << "\"" << region_name << "\", ";
     stream << "\"" << node.debug_info().filename() << "\", ";
     stream << node.debug_info().start_line() << ", ";
     stream << node.debug_info().end_line() << ", ";
     stream << node.debug_info().start_column() << ", ";
-    stream << node.debug_info().end_column() << ");" << std::endl;
+    stream << node.debug_info().end_column();
+
+    if (has_metadata) {
+        stream << ", ";
+        stream << "\"" << schedule_.sdfg().metadata("source_file") << "\", ";
+
+        std::string features_path = schedule_.sdfg().metadata("features_path") + "/" + region_name + ".npz";
+        stream << "\"" << features_path << "\"";
+    }
+    
+    stream << ");" << std::endl;
 }
 
 }
