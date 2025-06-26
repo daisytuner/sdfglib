ae43a22c4d95579978780a429e0480e92cde6eac
Adrian Schmitz <36474667+Atrisan@users.noreply.github.com>
2025-06-26 16:40:57 +0200

    localized memrange analysis (#109)

--- src/analysis/mem_access_range_analysis.cpp
+++ src/analysis/mem_access_range_analysis.cpp
@@ -7,47 +7,38 @@
 #include <symengine/infinity.h>
 #include <symengine/number.h>
 #include <symengine/symengine_rcp.h>
+
 #include <tuple>
+#include <unordered_set>
 #include <utility>
 #include <vector>
-#include "sdfg/analysis/mem_access_range_analysis_internal.h"
 
 #include "sdfg/analysis/analysis.h"
 #include "sdfg/analysis/assumptions_analysis.h"
+#include "sdfg/analysis/mem_access_range_analysis_internal.h"
 #include "sdfg/analysis/users.h"
-#include "sdfg/symbolic/symbolic.h"
-#include "sdfg/visualizer/dot_visualizer.h"
-
 #include "sdfg/symbolic/extreme_values.h"
+#include "sdfg/symbolic/symbolic.h"
 
 namespace sdfg {
 namespace analysis {
 
-MemAccessRanges::MemAccessRanges(StructuredSDFG& sdfg)
-    : Analysis(sdfg), node_(sdfg.root()), graph_() {
-}
+MemAccessRanges::MemAccessRanges(StructuredSDFG& sdfg) : Analysis(sdfg), graph_() {}
 
-void MemAccessRanges::run(analysis::AnalysisManager& analysis_manager) {
+void MemAccessRanges::run(structured_control_flow::ControlFlowNode& node,
+                          std::unordered_set<std::string> target_containers) {
+    auto& users = analysis_manager_->get<Users>();
+    auto& assumptions_analysis = analysis_manager_->get<AssumptionsAnalysis>();
 
-    auto& users = analysis_manager.get<Users>();
-    auto& assumptions_analysis = analysis_manager.get<AssumptionsAnalysis>();
+    UsersView users_view(users, node);
 
-    auto builder = MemAccessRangesBuilder(sdfg_, users, assumptions_analysis);
+    auto builder = MemAccessRangesBuilder(sdfg_, users_view, assumptions_analysis);
 
     auto& worklist = builder.worklist_;
 
-    // Collect argument names
-    for (auto& arg : sdfg_.arguments()) {
-        if (sdfg_.type(arg).type_id() != types::TypeID::Scalar) {
-            worklist.push_back(new WorkItem{&arg});
-        }
-    }
-
-    // Collect external names
-    for (auto& ext : sdfg_.externals()) {
-        if (sdfg_.type(ext).type_id() != types::TypeID::Scalar) {
-            worklist.push_back(new WorkItem{&ext});
-        }
+    // Initialize worklist with containers
+    for (const auto& container : target_containers) {
+        worklist.push_back(new WorkItem{&container});
     }
 
     // Iterate over all variables and their users
@@ -58,60 +49,93 @@ void MemAccessRanges::run(analysis::AnalysisManager& analysis_manager) {
         delete workItem;
     }
 
-    this->ranges_ = std::move(builder.ranges_);
+    this->ranges_.insert({&node, std::move(builder.ranges_)});
+}
+
+void MemAccessRanges::run(analysis::AnalysisManager& analysis_manager) {
+    this->analysis_manager_ = &analysis_manager;
+    std::unordered_set<std::string> containers;
+
+    // Collect argument names
+    for (auto& arg : sdfg_.arguments()) {
+        if (sdfg_.type(arg).type_id() != types::TypeID::Scalar) {
+            containers.insert(arg);
+        }
+    }
+
+    // Collect external names
+    for (auto& ext : sdfg_.externals()) {
+        if (sdfg_.type(ext).type_id() != types::TypeID::Scalar) {
+            containers.insert(ext);
+        }
+    }
+
+    this->run(sdfg_.root(), containers);
 }
 
 const MemAccessRange* MemAccessRanges::get(const std::string& varName) const {
-    auto res = ranges_.find(varName);
-    if (res != ranges_.end()) {
+    auto ranges = this->ranges_.find(&sdfg_.root());
+    if (ranges == this->ranges_.end()) {
+        return nullptr;
+    }
+    auto res = ranges->second.find(varName);
+    if (res != ranges->second.end()) {
         return &res->second;
     } else {
         return nullptr;
     }
 }
 
+const MemAccessRange* MemAccessRanges::get(const std::string& varName,
+                                           structured_control_flow::ControlFlowNode& node,
+                                           std::unordered_set<std::string> target_nodes) {
+    auto ranges = this->ranges_.find(&node);
+    if (ranges == this->ranges_.end()) {
+        this->run(node, target_nodes);
+        ranges = this->ranges_.find(&node);
+        if (ranges == this->ranges_.end()) {
+            return nullptr;
+        }
+    }
+    auto res = ranges->second.find(varName);
+    if (res != ranges->second.end()) {
+        return &res->second;
+    } else {
+        return nullptr;
+    }
+}
 
 MemAccessRange::MemAccessRange(
-    const std::string& name,
-    bool saw_read,
-    bool saw_write,
-    bool undefined,
-    const std::vector<std::pair<symbolic::Expression, symbolic::Expression>>&& dims
-): name_(name), saw_read_(saw_read), saw_write_(saw_write), undefined_(undefined), dims_(dims) {}
+    const std::string& name, bool saw_read, bool saw_write, bool undefined,
+    const std::vector<std::pair<symbolic::Expression, symbolic::Expression>>&& dims)
+    : name_(name), saw_read_(saw_read), saw_write_(saw_write), undefined_(undefined), dims_(dims) {}
 
-const std::string& MemAccessRange::get_name() const {
-    return name_;
-}
+const std::string& MemAccessRange::get_name() const { return name_; }
 
-bool MemAccessRange::saw_read() const {
-    return saw_read_;
-}
-bool MemAccessRange::saw_write() const {
-    return saw_write_;
-}
-bool MemAccessRange::is_undefined() const {
-    return undefined_;
-}
+bool MemAccessRange::saw_read() const { return saw_read_; }
+bool MemAccessRange::saw_write() const { return saw_write_; }
+bool MemAccessRange::is_undefined() const { return undefined_; }
 
-const std::vector<std::pair<symbolic::Expression, symbolic::Expression>>& MemAccessRange::dims() const {
+const std::vector<std::pair<symbolic::Expression, symbolic::Expression>>& MemAccessRange::dims()
+    const {
     return dims_;
 }
 
 void MemAccessRangesBuilder::process_workItem(WorkItem* item) {
     const auto* varName = item->var_name;
     const auto& type = sdfg_.type(*varName);
-    
+
     const auto& reads = users_.reads(*varName);
-    process_direct_users(item, varName, false, reads);
+    process_direct_users(item, false, reads);
 
     const auto& writes = users_.writes(*varName);
-    process_direct_users(item, varName, true, writes);
+    process_direct_users(item, true, writes);
 
     const auto& views = users_.views(*varName);
     if (!views.empty()) {
-        #ifndef NDEBUG
+#ifndef NDEBUG
         std::cerr << "Found views for " << *varName << " => not rangeable!" << std::endl;
-        #endif
+#endif
         item->undefined = true;
     }
 
@@ -122,17 +146,20 @@ void MemAccessRangesBuilder::process_workItem(WorkItem* item) {
     }
 
     if (!item->dims.empty()) {
-
         std::vector<std::pair<symbolic::Expression, symbolic::Expression>> finalDims;
         finalDims.reserve(item->dims.size());
 
-        for (auto& dim: item->dims) {
+        for (auto& dim : item->dims) {
             auto& lowerExprs = std::get<0>(dim);
             bool isLowerUndefined = std::get<1>(dim);
-            symbolic::Expression lb = (!lowerExprs.empty() && !isLowerUndefined)? SymEngine::min(lowerExprs) : SymEngine::RCP<const SymEngine::Basic>();
+            symbolic::Expression lb = (!lowerExprs.empty() && !isLowerUndefined)
+                                          ? SymEngine::min(lowerExprs)
+                                          : SymEngine::RCP<const SymEngine::Basic>();
             auto& upperExprs = std::get<2>(dim);
             bool isUpperUndefined = std::get<3>(dim);
-            symbolic::Expression ub = (!upperExprs.empty() && !isUpperUndefined)? SymEngine::max(upperExprs) : SymEngine::RCP<const SymEngine::Basic>();
+            symbolic::Expression ub = (!upperExprs.empty() && !isUpperUndefined)
+                                          ? SymEngine::max(upperExprs)
+                                          : SymEngine::RCP<const SymEngine::Basic>();
 
             if (lb.is_null() || ub.is_null()) {
                 item->undefined = true;
@@ -150,28 +177,27 @@ void MemAccessRangesBuilder::process_workItem(WorkItem* item) {
         }
 
 #if !defined(NDEBUG)
-        // std::cout << "RangeAna: " << *varName << ": (" << (item->saw_read? "r" : "") << (item->saw_write? "w" : "") << ") " << (!item->undefined? "ranged" : "undef")
+        // std::cout << "RangeAna: " << *varName << ": (" << (item->saw_read? "r" : "") <<
+        // (item->saw_write? "w" : "") << ") " << (!item->undefined? "ranged" : "undef")
         //           << ", " << finalDims.size() << "D: [\n";
         // for (size_t i = 0; i < finalDims.size(); ++i) {
         //     const auto& dim = finalDims[i];
-        //     std::cout << "\t(" << (!dim.first.is_null()? dim.first->__str__() : "null") 
-        //               << " .. " << (!dim.second.is_null()? dim.second->__str__() : "null") << ")";
+        //     std::cout << "\t(" << (!dim.first.is_null()? dim.first->__str__() : "null")
+        //               << " .. " << (!dim.second.is_null()? dim.second->__str__() : "null") <<
+        //               ")";
         //     std::cout << "\n";
         // }
         // std::cout << "]" << std::endl;
 #endif
 
-        this->ranges_.emplace(
-            std::piecewise_construct,
-            std::forward_as_tuple(*varName),
-            std::forward_as_tuple(*varName, item->saw_read, item->saw_write, item->undefined, std::move(finalDims))
-        );
+        this->ranges_.emplace(std::piecewise_construct, std::forward_as_tuple(*varName),
+                              std::forward_as_tuple(*varName, item->saw_read, item->saw_write,
+                                                    item->undefined, std::move(finalDims)));
     }
 }
 
-void MemAccessRangesBuilder::process_direct_users(WorkItem* item, const std::string* varName, bool is_write, std::vector<User*> accesses) {
-
-    const auto accessTypeStr = is_write? "w" : "r";
+void MemAccessRangesBuilder::process_direct_users(WorkItem* item, bool is_write,
+                                                  std::vector<User*> accesses) {
     for (auto& access : accesses) {
         auto subsets = access->subsets();
         const auto& user_scope = analysis::Users::scope(access);
@@ -185,7 +211,9 @@ void MemAccessRangesBuilder::process_direct_users(WorkItem* item, const std::str
             auto subsetDims = subset.size();
             item->dims.reserve(subsetDims);
             for (size_t i = item->dims.size(); i < subsetDims; ++i) {
-                item->dims.emplace_back(std::make_tuple<std::vector<symbolic::Expression>, bool, std::vector<symbolic::Expression>, bool>({}, false, {}, false));
+                item->dims.emplace_back(
+                    std::make_tuple<std::vector<symbolic::Expression>, bool,
+                                    std::vector<symbolic::Expression>, bool>({}, false, {}, false));
             }
             int dimIdx = 0;
             for (auto& dim : subset) {
