874cc34dacf8c620ce88d4d0d090b11e951fc19a
Lukas Truemper <lukas.truemper@outlook.de>
2025-07-02 11:53:23 +0200

    simplifies condition propagation passes

--- include/sdfg/passes/symbolic/condition_propagation.h
+++ include/sdfg/passes/symbolic/condition_propagation.h
@@ -5,22 +5,6 @@
 namespace sdfg {
 namespace passes {
 
-class ForwardConditionPropagation : public Pass {
-   private:
-    bool propagate_condition(builder::StructuredSDFGBuilder& builder,
-                             analysis::AnalysisManager& analysis_manager,
-                             structured_control_flow::Sequence& parent,
-                             const symbolic::Condition& condition);
-
-   public:
-    ForwardConditionPropagation();
-
-    virtual std::string name() override;
-
-    virtual bool run_pass(builder::StructuredSDFGBuilder& builder,
-                          analysis::AnalysisManager& analysis_manager) override;
-};
-
 class BackwardConditionPropagation : public Pass {
    private:
     bool eliminate_condition(builder::StructuredSDFGBuilder& builder,
