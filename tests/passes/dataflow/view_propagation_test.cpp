efe1170e1a701d5eb3e63d5acec3714f83ec451d
Lukas Truemper <lukas.truemper@outlook.de>
2025-07-21 13:48:17 +0200

    adds basic test cases for view propagation

--- tests/passes/dataflow/view_propagation_test.cpp
+++ tests/passes/dataflow/view_propagation_test.cpp
@@ -8,7 +8,7 @@
 
 using namespace sdfg;
 
-TEST(ViewPropagationTest, Simple) {
+TEST(ViewPropagationTest, ReferenceMemlet) {
     builder::StructuredSDFGBuilder builder("sdfg", FunctionType_CPU);
 
     types::Scalar desc(types::PrimitiveType::Double);
@@ -18,14 +18,16 @@ TEST(ViewPropagationTest, Simple) {
 
     auto& root = builder.subject().root();
     auto& block1 = builder.add_block(root);
-    auto& viewed_node = builder.add_access(block1, "A");
-    auto& viewing_node = builder.add_access(block1, "a");
-    builder.add_memlet(block1, viewed_node, "void", viewing_node, "refs", {symbolic::integer(0)});
+    auto& a_input = builder.add_access(block1, "A");
+    auto& a_output = builder.add_access(block1, "a");
+    builder.add_memlet(block1, a_input, "void", a_output, "ref", {symbolic::integer(0)});
 
     auto& block2 = builder.add_block(root);
+    auto& input_node = builder.add_access(block2, "a");
     auto& output_node = builder.add_access(block2, "a");
     auto& tasklet =
-        builder.add_tasklet(block2, data_flow::TaskletCode::assign, {"_out", desc}, {{"0", desc}});
+        builder.add_tasklet(block2, data_flow::TaskletCode::add, {"_out", desc}, {{"_in0", desc}, {"1", desc}});
+    builder.add_memlet(block2, input_node, "void", tasklet, "_in0", {symbolic::integer(0)});
     builder.add_memlet(block2, tasklet, "_out", output_node, "void", {symbolic::integer(0)});
 
     auto sdfg = builder.move();
@@ -34,9 +36,79 @@ TEST(ViewPropagationTest, Simple) {
     builder::StructuredSDFGBuilder builder_opt(sdfg);
     analysis::AnalysisManager analysis_manager(builder_opt.subject());
     passes::ViewPropagation pass;
-    pass.run(builder_opt, analysis_manager);
+    EXPECT_TRUE(pass.run(builder_opt, analysis_manager));
     sdfg = builder_opt.move();
 
     // Check result
+    EXPECT_EQ(input_node.data(), "A");
     EXPECT_EQ(output_node.data(), "A");
+}
+
+TEST(ViewPropagationTest, DereferenceMemlet_Load) {
+    builder::StructuredSDFGBuilder builder("sdfg", FunctionType_CPU);
+
+    types::Scalar desc(types::PrimitiveType::Double);
+    types::Pointer desc_ptr(desc);
+    types::Pointer desc_ptr_2(static_cast<const types::IType&>(desc_ptr));
+    builder.add_container("A", desc_ptr_2, true);
+    builder.add_container("a", desc_ptr);
+
+    auto& root = builder.subject().root();
+    auto& block1 = builder.add_block(root);
+    auto& a_input = builder.add_access(block1, "A");
+    auto& a_output = builder.add_access(block1, "a");
+    builder.add_memlet(block1, a_input, "void", a_output, "deref", {symbolic::integer(0)});
+
+    auto& block2 = builder.add_block(root);
+    auto& output_node = builder.add_access(block2, "a");
+    auto& tasklet = builder.add_tasklet(block2, data_flow::TaskletCode::assign, {"_out", desc}, {{"0", desc}});
+    builder.add_memlet(block2, tasklet, "_out", output_node, "void", {symbolic::integer(0)});
+
+    auto sdfg = builder.move();
+
+    // Apply pass
+    builder::StructuredSDFGBuilder builder_opt(sdfg);
+    analysis::AnalysisManager analysis_manager(builder_opt.subject());
+    passes::ViewPropagation pass;
+    EXPECT_FALSE(pass.run(builder_opt, analysis_manager));
+
+    sdfg = builder_opt.move();
+
+    // Check result
+    EXPECT_EQ(output_node.data(), "a");
+}
+
+TEST(ViewPropagationTest, DereferenceMemlet_Store) {
+    builder::StructuredSDFGBuilder builder("sdfg", FunctionType_CPU);
+
+    types::Scalar desc(types::PrimitiveType::Double);
+    types::Pointer desc_ptr(desc);
+    types::Pointer desc_ptr_2(static_cast<const types::IType&>(desc_ptr));
+    builder.add_container("A", desc_ptr_2, true);
+    builder.add_container("a", desc_ptr);
+
+    auto& root = builder.subject().root();
+
+    auto& block1 = builder.add_block(root);
+    auto& output_node = builder.add_access(block1, "a");
+    auto& tasklet = builder.add_tasklet(block1, data_flow::TaskletCode::assign, {"_out", desc}, {{"0", desc}});
+    builder.add_memlet(block1, tasklet, "_out", output_node, "void", {symbolic::integer(0)});
+
+    auto& block2 = builder.add_block(root);
+    auto& a_input = builder.add_access(block2, "a");
+    auto& a_output = builder.add_access(block2, "A");
+    builder.add_memlet(block2, a_input, "deref", a_output, "void", {symbolic::integer(0)});
+
+    auto sdfg = builder.move();
+
+    // Apply pass
+    builder::StructuredSDFGBuilder builder_opt(sdfg);
+    analysis::AnalysisManager analysis_manager(builder_opt.subject());
+    passes::ViewPropagation pass;
+    EXPECT_FALSE(pass.run(builder_opt, analysis_manager));
+
+    sdfg = builder_opt.move();
+
+    // Check result
+    EXPECT_EQ(output_node.data(), "a");
 }
