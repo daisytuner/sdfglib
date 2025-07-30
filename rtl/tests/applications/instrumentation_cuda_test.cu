d1adc39a43c66c9f46bb0905b970d7e1e750325f
Lukas Truemper <lukas.truemper@outlook.de>
2025-07-30 14:06:10 +0200

    adds tests for cuda perf counters with papi and rtl

---
+++ rtl/tests/applications/instrumentation_cuda_test.cu
@@ -0,0 +1,70 @@
+#include <cuda_runtime.h>
+#include <daisy_rtl/daisy_rtl.h>
+#include <stdio.h>
+
+// CUDA kernels -----------------------------------------------------------
+__global__ void initKernel(double *A, double *B, double *C, int N) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < N) {
+        A[idx] = idx;
+        B[idx] = N - idx;
+        C[idx] = 0.0;
+    }
+}
+
+__global__ void addKernel(const double *A, const double *B, double *C, int N) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < N) {
+        C[idx] = A[idx] + B[idx];
+    }
+}
+
+int main(int argc, char **argv) {
+    const int N = 1000;
+    const size_t bytes = N * sizeof(double);
+
+    double *hC = (double *) malloc(bytes);
+
+    double *dA, *dB, *dC;
+    cudaMalloc(&dA, bytes);
+    cudaMalloc(&dB, bytes);
+    cudaMalloc(&dC, bytes);
+
+    dim3 blockSize = {256, 1, 1};
+    dim3 gridSize = {(unsigned int) ((N + blockSize.x - 1) / blockSize.x), 1, 1};
+
+    __daisy_instrumentation_t *context = __daisy_instrumentation_init();
+
+    __daisy_metadata_t metadata = {
+        .file_name = "instrumentation_cuda_test.cu",
+        .function_name = "main",
+        .line_begin = 18,
+        .line_end = 31,
+        .column_begin = 4,
+        .column_end = 5,
+        .region_name = "instrumentation_cuda_test_main",
+    };
+
+    for (size_t rep = 0; rep < 10; rep++) {
+        __daisy_instrumentation_enter(context, &metadata);
+
+        initKernel<<<gridSize, blockSize>>>(dA, dB, dC, N);
+        addKernel<<<gridSize, blockSize>>>(dA, dB, dC, N);
+        cudaDeviceSynchronize();
+
+        __daisy_instrumentation_exit(context, &metadata);
+
+        // Copy result back to host for verification/printing
+        cudaMemcpy(hC, dC, bytes, cudaMemcpyDeviceToHost);
+        for (int i = 0; i < N; i++) {
+            printf("%f\n", hC[i]);
+        }
+    }
+
+    cudaFree(dA);
+    cudaFree(dB);
+    cudaFree(dC);
+    free(hC);
+
+    __daisy_instrumentation_finalize(context);
+}
--- rtl/tests/rtl_tests.py
+++ rtl/tests/rtl_tests.py
@@ -9,7 +9,7 @@ from pathlib import Path
     "event",
     [
         pytest.param("DURATION_TIME"),
-        pytest.param("perf::INSTRUCTIONS,perf::CYCLES"),
+        pytest.param("perf::BRANCHES,perf::CYCLES"),
     ],
 )
 def test_instrumentation(event):
@@ -32,12 +32,79 @@ def test_instrumentation(event):
         universal_newlines=True,
     )
     stdout, stderr = process.communicate()
+    print(stdout)
+    print(stderr)
     assert process.returncode == 0
 
     (workdir / "data.json").unlink(missing_ok=True)
 
     ## THIS VERSION IS RUNNER-SPECIFIC and points to CI version of PAPI
-    os.environ["__DAISY_PAPI_VERSION"] = "0x07010000"
+    os.environ["__DAISY_PAPI_VERSION"] = "0x07020000"
+    os.environ["__DAISY_INSTRUMENTATION_FILE"] = str(workdir / "data.json")
+    os.environ["__DAISY_INSTRUMENTATION_EVENTS"] = event
+
+    # Run benchmark
+    process = subprocess.Popen(
+        [str(output_path)],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+    )
+    stdout, stderr = process.communicate()
+    print(stdout)
+    print(stderr)
+    assert process.returncode == 0
+
+    event_names = event.split(",")
+
+    result = json.load(open(workdir / "data.json"))
+    events = result["traceEvents"]
+    for i in range(len(events)):
+        assert events[i]["name"] == "instrumentation_test_main"
+        assert events[i]["cat"] == "DAISY"
+        assert events[i]["ph"] == "X"
+        assert events[i]["args"]["file"] == "instrumentation_test.c"
+        assert events[i]["args"]["function"] == "main"
+        assert events[i]["args"]["line_begin"] == 18
+        assert events[i]["args"]["line_end"] == 31
+        assert events[i]["args"]["column_begin"] == 4
+        assert events[i]["args"]["column_end"] == 5
+        for event_name in event_names:
+            assert event_name in events[i]["args"]
+
+@pytest.mark.parametrize(
+    "event",
+    [
+        pytest.param("DURATION_TIME"),
+        pytest.param("cuda:::dram__bytes,cuda:::sm__inst_executed_pipe_fma"),
+    ],
+)
+def test_instrumentation_cuda(event):
+    workdir = Path(__file__).parent / "applications"
+
+    benchmark_path = workdir / "instrumentation_cuda_test.cu"
+    output_path = workdir / "instrumentation_cuda_test.out"
+    cmd = [
+        "nvcc",
+        str(benchmark_path),
+        "-o",
+        str(output_path),
+        "-ldaisy_rtl",
+    ]
+
+    process = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+    )
+    stdout, stderr = process.communicate()
+    assert process.returncode == 0
+
+    (workdir / "data.json").unlink(missing_ok=True)
+
+    ## THIS VERSION IS RUNNER-SPECIFIC and points to CI version of PAPI
+    os.environ["__DAISY_PAPI_VERSION"] = "0x07020000"
     os.environ["__DAISY_INSTRUMENTATION_FILE"] = str(workdir / "data.json")
     os.environ["__DAISY_INSTRUMENTATION_EVENTS"] = event
 
@@ -56,10 +123,10 @@ def test_instrumentation(event):
     result = json.load(open(workdir / "data.json"))
     events = result["traceEvents"]
     for i in range(len(events)):
-        assert events[i]["name"] == "instrumentation_test_main"
+        assert events[i]["name"] == "instrumentation_cuda_test_main"
         assert events[i]["cat"] == "DAISY"
         assert events[i]["ph"] == "X"
-        assert events[i]["args"]["file"] == "instrumentation_test.c"
+        assert events[i]["args"]["file"] == "instrumentation_cuda_test.cu"
         assert events[i]["args"]["function"] == "main"
         assert events[i]["args"]["line_begin"] == 18
         assert events[i]["args"]["line_end"] == 31
