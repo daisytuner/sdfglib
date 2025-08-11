828f7bc72363689276eb517310eee0a46fa10947
Lukas Truemper <lukas.truemper@outlook.de>
2025-08-11 21:51:47 +0200

    moves transition in condition elimination

--- include/sdfg/passes/structured_control_flow/condition_elimination.h
+++ include/sdfg/passes/structured_control_flow/condition_elimination.h
@@ -8,12 +8,7 @@ namespace passes {
 
 class ConditionElimination : public visitor::StructuredSDFGVisitor {
 private:
-    bool eliminate_condition(
-        structured_control_flow::Sequence& root,
-        structured_control_flow::IfElse& match,
-        structured_control_flow::StructuredLoop& loop,
-        const symbolic::Condition& condition
-    );
+    bool eliminate_condition(structured_control_flow::Sequence& root, structured_control_flow::IfElse& match);
 
 public:
     ConditionElimination(builder::StructuredSDFGBuilder& builder, analysis::AnalysisManager& analysis_manager);
--- src/passes/structured_control_flow/condition_elimination.cpp
+++ src/passes/structured_control_flow/condition_elimination.cpp
@@ -6,28 +6,34 @@
 namespace sdfg {
 namespace passes {
 
-bool ConditionElimination::eliminate_condition(
-    structured_control_flow::Sequence& root,
-    structured_control_flow::IfElse& match,
-    structured_control_flow::StructuredLoop& loop,
-    const symbolic::Condition& condition
-) {
+bool ConditionElimination::
+    eliminate_condition(structured_control_flow::Sequence& root, structured_control_flow::IfElse& match) {
+    auto branch = match.at(0);
+    auto& branch_root = branch.first;
+    auto& branch_condition = branch.second;
+    auto& loop = static_cast<structured_control_flow::StructuredLoop&>(branch_root.at(0).first);
+
     auto loop_indvar = loop.indvar();
     auto loop_init = loop.init();
     auto loop_condition = loop.condition();
 
     // indvar shall not be read afterwards directly since loop init overrides it
     analysis::Users& users = this->analysis_manager_.get<analysis::Users>();
-    analysis::UsersView loop_users(users, match.at(0).first);
+    analysis::UsersView loop_users(users, branch_root);
     if (users.reads(loop_indvar->get_name()).size() > loop_users.reads(loop_indvar->get_name()).size()) {
         return false;
     }
 
     // If loop condition == true => condition == true && condition == false => loop condition == false
     auto loop_iter0_condition = symbolic::subs(loop_condition, loop_indvar, loop_init);
-    if (symbolic::eq(loop_iter0_condition, condition)) {
-        auto& new_seq = this->builder_.add_sequence_before(root, match).first;
-        this->builder_.insert(loop, match.at(0).first, new_seq, match.debug_info());
+    if (symbolic::eq(loop_iter0_condition, branch_condition)) {
+        // Insert placeholder before if-else
+        auto new_child = this->builder_.add_sequence_before(root, match);
+
+        // Move children of branch to placeholder
+        this->builder_.insert_children(new_child.first, branch_root, 0);
+
+        // Remove now empty if-else
         this->builder_.remove_child(root, match);
 
         return true;
@@ -58,9 +64,8 @@ bool ConditionElimination::accept(structured_control_flow::Sequence& parent, str
     if (!dynamic_cast<structured_control_flow::StructuredLoop*>(&root.at(0).first)) {
         return false;
     }
-    auto& loop = static_cast<structured_control_flow::StructuredLoop&>(root.at(0).first);
 
-    return this->eliminate_condition(parent, node, loop, condition);
+    return this->eliminate_condition(parent, node);
 };
 
 } // namespace passes
