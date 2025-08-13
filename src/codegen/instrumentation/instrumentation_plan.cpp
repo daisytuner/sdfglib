6832d204d04d84b4ef8cec8389628d80c4aac062
Lukas Truemper <lukas.truemper@outlook.de>
2025-08-13 19:48:54 +0200

    extended tracing format and fixes for instrumentation codegen

--- src/codegen/instrumentation/instrumentation_plan.cpp
+++ src/codegen/instrumentation/instrumentation_plan.cpp
@@ -29,15 +29,25 @@ void InstrumentationPlan::begin_instrumentation(const structured_control_flow::C
     stream << metdata_var << ".column_begin = " << dbg_info.start_column() << ";" << std::endl;
     stream << metdata_var << ".column_end = " << dbg_info.end_column() << ";" << std::endl;
 
-    stream << "__daisy_instrumentation_enter(__daisy_instrumentation_ctx, &" << metdata_var << ", "
-           << this->nodes_.at(&node) << ");" << std::endl;
+    if (this->nodes_.at(&node) == InstrumentationEventType::CPU) {
+        stream << "__daisy_instrumentation_enter(__daisy_instrumentation_ctx, &" << metdata_var << ", "
+               << "__DAISY_EVENT_SET_CPU" << ");" << std::endl;
+    } else {
+        stream << "__daisy_instrumentation_enter(__daisy_instrumentation_ctx, &" << metdata_var << ", "
+               << "__DAISY_EVENT_SET_CUDA" << ");" << std::endl;
+    }
 }
 
 void InstrumentationPlan::end_instrumentation(const structured_control_flow::ControlFlowNode& node, PrettyPrinter& stream)
     const {
     std::string metdata_var = sdfg_.name() + "_" + std::to_string(node.element_id());
-    stream << "__daisy_instrumentation_exit(__daisy_instrumentation_ctx, &" << metdata_var << ", "
-           << this->nodes_.at(&node) << ");" << std::endl;
+    if (this->nodes_.at(&node) == InstrumentationEventType::CPU) {
+        stream << "__daisy_instrumentation_exit(__daisy_instrumentation_ctx, &" << metdata_var << ", "
+               << "__DAISY_EVENT_SET_CPU" << ");" << std::endl;
+    } else {
+        stream << "__daisy_instrumentation_exit(__daisy_instrumentation_ctx, &" << metdata_var << ", "
+               << "__DAISY_EVENT_SET_CUDA" << ");" << std::endl;
+    }
 }
 
 std::unique_ptr<InstrumentationPlan> InstrumentationPlan::none(StructuredSDFG& sdfg) {
@@ -53,7 +63,13 @@ std::unique_ptr<InstrumentationPlan> InstrumentationPlan::outermost_loops_plan(S
 
     std::unordered_map<const structured_control_flow::ControlFlowNode*, InstrumentationEventType> nodes;
     for (auto loop : ols) {
-        nodes.insert({loop, InstrumentationEventType::CPU});
+        if (auto map_node = dynamic_cast<const structured_control_flow::Map*>(loop)) {
+            if (map_node->schedule_type().value() == "CUDA") {
+                nodes.insert({loop, InstrumentationEventType::CUDA});
+                continue;
+            }
+        }
+        nodes.insert({loop, InstrumentationEventType::CPU}); // Default to CPU if not CUDA
     }
 
     return std::make_unique<InstrumentationPlan>(sdfg, nodes);
