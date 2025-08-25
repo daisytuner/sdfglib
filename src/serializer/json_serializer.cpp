c152fddb89bca5e36d51e62d08796b0c98051dff
Lukas Truemper <lukas.truemper@outlook.de>
2025-08-25 11:07:22 +0200

    Adds batch and layer normalization layers

--- src/serializer/json_serializer.cpp
+++ src/serializer/json_serializer.cpp
@@ -1160,6 +1160,10 @@ void register_default_serializers() {
             return std::make_unique<math::ml::AddNodeSerializer>();
         });
     LibraryNodeSerializerRegistry::instance()
+        .register_library_node_serializer(math::ml::LibraryNodeType_BatchNormalization.value(), []() {
+            return std::make_unique<math::ml::BatchNormalizationNodeSerializer>();
+        });
+    LibraryNodeSerializerRegistry::instance()
         .register_library_node_serializer(math::ml::LibraryNodeType_Clip.value(), []() {
             return std::make_unique<math::ml::ClipNodeSerializer>();
         });
@@ -1190,6 +1194,10 @@ void register_default_serializers() {
     LibraryNodeSerializerRegistry::instance()
         .register_library_node_serializer(math::ml::LibraryNodeType_HardSigmoid.value(), []() {
             return std::make_unique<math::ml::HardSigmoidNodeSerializer>();
+        });
+    LibraryNodeSerializerRegistry::instance()
+        .register_library_node_serializer(math::ml::LibraryNodeType_LayerNormalization.value(), []() {
+            return std::make_unique<math::ml::LayerNormalizationNodeSerializer>();
         });
     LibraryNodeSerializerRegistry::instance()
         .register_library_node_serializer(math::ml::LibraryNodeType_LeakyReLU.value(), []() {
