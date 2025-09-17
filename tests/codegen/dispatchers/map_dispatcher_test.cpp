2ddae77cb7b05647aacc48216fe2e94bcc5db5b6
Adrian Schmitz <36474667+Atrisan@users.noreply.github.com>
2025-09-17 14:42:46 +0200

    Revert "Omp interface fix (#222)" (#229)
    This reverts commit 0b5356da9196c5afb70aef5eb07f65f7024d08a5.

--- tests/codegen/dispatchers/map_dispatcher_test.cpp
+++ tests/codegen/dispatchers/map_dispatcher_test.cpp
@@ -4,7 +4,6 @@
 
 #include "sdfg/builder/structured_sdfg_builder.h"
 #include "sdfg/codegen/language_extensions/c_language_extension.h"
-#include "sdfg/structured_control_flow/map.h"
 
 using namespace sdfg;
 
@@ -37,107 +36,4 @@ TEST(MapDispatcherTest, DispatchNode) {
     EXPECT_EQ(globals_stream.str(), "");
     EXPECT_TRUE(library_factory.snippets().empty());
     EXPECT_EQ(main_stream.str(), "// Map\nfor(i = 0;i < 10;i = 1 + i)\n{\n}\n");
-}
-
-TEST(CPU_PARALLELMapDispatcherTest, DispatchNode) {
-    builder::StructuredSDFGBuilder builder("sdfg_a", FunctionType_CPU);
-    auto& sdfg = builder.subject();
-    auto& root = sdfg.root();
-
-    builder.add_container("i", types::Scalar(types::PrimitiveType::Int32));
-    auto& loop = builder.add_map(
-        root,
-        symbolic::symbol("i"),
-        symbolic::Lt(symbolic::symbol("i"), symbolic::integer(10)),
-        symbolic::integer(0),
-        symbolic::add(symbolic::symbol("i"), symbolic::integer(1)),
-        structured_control_flow::ScheduleType_CPU_Parallel::create()
-    );
-
-    auto final_sdfg = builder.move();
-
-    codegen::CLanguageExtension language_extension;
-    auto instrumentation = codegen::InstrumentationPlan::none(*final_sdfg);
-    codegen::CPUParallelMapDispatcher dispatcher(language_extension, *final_sdfg, loop, *instrumentation);
-
-    codegen::PrettyPrinter main_stream;
-    codegen::PrettyPrinter globals_stream;
-    codegen::CodeSnippetFactory library_factory;
-    dispatcher.dispatch_node(main_stream, globals_stream, library_factory);
-
-    EXPECT_EQ(globals_stream.str(), "");
-    EXPECT_TRUE(library_factory.snippets().empty());
-    EXPECT_EQ(main_stream.str(), "#pragma omp parallel for schedule(static)\n// Map\nfor(i = 0;i < 10;i = 1 + i)\n{\n}\n");
-}
-
-TEST(CPU_PARALLELMapDispatcherTest, DispatchNodeScheduleDynamic) {
-    builder::StructuredSDFGBuilder builder("sdfg_a", FunctionType_CPU);
-    auto& sdfg = builder.subject();
-    auto& root = sdfg.root();
-
-    builder.add_container("i", types::Scalar(types::PrimitiveType::Int32));
-    auto& loop = builder.add_map(
-        root,
-        symbolic::symbol("i"),
-        symbolic::Lt(symbolic::symbol("i"), symbolic::integer(10)),
-        symbolic::integer(0),
-        symbolic::add(symbolic::symbol("i"), symbolic::integer(1)),
-        structured_control_flow::ScheduleType_CPU_Parallel::create()
-    );
-
-    ScheduleType_CPU_Parallel::omp_schedule(loop.schedule_type(), OpenMPSchedule::Dynamic);
-
-    auto final_sdfg = builder.move();
-
-    codegen::CLanguageExtension language_extension;
-    auto instrumentation = codegen::InstrumentationPlan::none(*final_sdfg);
-    codegen::CPUParallelMapDispatcher dispatcher(language_extension, *final_sdfg, loop, *instrumentation);
-
-    codegen::PrettyPrinter main_stream;
-    codegen::PrettyPrinter globals_stream;
-    codegen::CodeSnippetFactory library_factory;
-    dispatcher.dispatch_node(main_stream, globals_stream, library_factory);
-
-    EXPECT_EQ(globals_stream.str(), "");
-    EXPECT_TRUE(library_factory.snippets().empty());
-    EXPECT_EQ(
-        main_stream.str(), "#pragma omp parallel for schedule(dynamic)\n// Map\nfor(i = 0;i < 10;i = 1 + i)\n{\n}\n"
-    );
-}
-
-TEST(CPU_PARALLELMapDispatcherTest, DispatchNodeNumThreads) {
-    builder::StructuredSDFGBuilder builder("sdfg_a", FunctionType_CPU);
-    auto& sdfg = builder.subject();
-    auto& root = sdfg.root();
-
-    builder.add_container("i", types::Scalar(types::PrimitiveType::Int32));
-    auto& loop = builder.add_map(
-        root,
-        symbolic::symbol("i"),
-        symbolic::Lt(symbolic::symbol("i"), symbolic::integer(10)),
-        symbolic::integer(0),
-        symbolic::add(symbolic::symbol("i"), symbolic::integer(1)),
-        structured_control_flow::ScheduleType_CPU_Parallel::create()
-    );
-
-    ScheduleType_CPU_Parallel::omp_schedule(loop.schedule_type(), OpenMPSchedule::Dynamic);
-    ScheduleType_CPU_Parallel::num_threads(loop.schedule_type(), symbolic::integer(4));
-
-    auto final_sdfg = builder.move();
-
-    codegen::CLanguageExtension language_extension;
-    auto instrumentation = codegen::InstrumentationPlan::none(*final_sdfg);
-    codegen::CPUParallelMapDispatcher dispatcher(language_extension, *final_sdfg, loop, *instrumentation);
-
-    codegen::PrettyPrinter main_stream;
-    codegen::PrettyPrinter globals_stream;
-    codegen::CodeSnippetFactory library_factory;
-    dispatcher.dispatch_node(main_stream, globals_stream, library_factory);
-
-    EXPECT_EQ(globals_stream.str(), "");
-    EXPECT_TRUE(library_factory.snippets().empty());
-    EXPECT_EQ(
-        main_stream.str(),
-        "#pragma omp parallel for schedule(dynamic) num_threads(4)\n// Map\nfor(i = 0;i < 10;i = 1 + i)\n{\n}\n"
-    );
 }
