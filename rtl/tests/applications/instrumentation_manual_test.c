7292a9efa48e067e6825fd031fd0830056ca1054
Lukas Truemper <lukas.truemper@outlook.de>
2025-10-10 07:53:01 +0200

    adds static increment to instrumentation rtl

--- rtl/tests/applications/instrumentation_manual_test.c
+++ rtl/tests/applications/instrumentation_manual_test.c
@@ -10,12 +10,6 @@ void main(int argc, char** argv) {
         .line_end = 31,
         .column_begin = 4,
         .column_end = 5,
-        .sdfg_name = "__daisy_instrumentation_test_0",
-        .sdfg_file = "/tmp/DOCC/0000-0000/123456789/sdfg_0.json",
-        .element_id = 10,
-        .element_type = "for",
-        .target_type = "sequential",
-        .loopnest_index = 0,
         .region_uuid = "__daisy_instrumentation_test_0_10"
     };
     unsigned long long region_id = __daisy_instrumentation_init(&metadata, __DAISY_EVENT_SET_CPU);
@@ -38,6 +32,7 @@ void main(int argc, char** argv) {
         }
 
         __daisy_instrumentation_exit(region_id);
+        __daisy_instrumentation_increment(region_id, "foo", rep);
 
         for (int i = 0; i < 1000; i++) {
             printf("%f\n", C[i]);
