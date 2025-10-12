fcde94e50df87f72d36275caec24a068d4ca69d4
Lukas Truemper <lukas.truemper@outlook.de>
2025-10-12 18:45:33 +0200

    adds visitor test

--- tests/structured_control_flow/for_each_test.cpp
+++ tests/structured_control_flow/for_each_test.cpp
@@ -5,6 +5,7 @@
 #include "sdfg/codegen/language_extensions/c_language_extension.h"
 #include "sdfg/serializer/json_serializer.h"
 #include "sdfg/symbolic/symbolic.h"
+#include "sdfg/visitor/structured_sdfg_visitor.h"
 
 using namespace sdfg;
 
@@ -68,4 +69,34 @@ TEST(ForEachTest, Dispatch) {
     EXPECT_EQ(globals_stream.str(), "");
     EXPECT_TRUE(library_factory.snippets().empty());
     EXPECT_EQ(main_stream.str(), "for(iter = *((void* *) list);iter != NULL;iter = *((void* *) iter))\n{\n}\n");
+}
+
+class ForEachVisitor : public visitor::StructuredSDFGVisitor {
+public:
+    ForEachVisitor(builder::StructuredSDFGBuilder& builder, analysis::AnalysisManager& analysis_manager)
+        : visitor::StructuredSDFGVisitor(builder, analysis_manager) {}
+
+    bool accept(structured_control_flow::ForEach& node) override { return true; };
+};
+
+TEST(StructuredSDFGVisitorTest, ForEach) {
+    builder::StructuredSDFGBuilder builder("sdfg_1", FunctionType_CPU);
+
+    auto& sdfg = builder.subject();
+    auto& root = sdfg.root();
+
+    // Add containers
+    types::Pointer opaque_desc;
+    builder.add_container("list", opaque_desc, true);
+    builder.add_container("iter", opaque_desc);
+
+    auto sym_iter = symbolic::symbol("iter");
+    auto sym_list = symbolic::symbol("list");
+    auto sym_nullptr = symbolic::__nullptr__();
+
+    auto& for_each = builder.add_for_each(root, sym_iter, sym_nullptr, sym_iter, sym_list);
+
+    analysis::AnalysisManager analysis_manager(builder.subject());
+    ForEachVisitor visitor(builder, analysis_manager);
+    EXPECT_TRUE(visitor.visit());
 }
