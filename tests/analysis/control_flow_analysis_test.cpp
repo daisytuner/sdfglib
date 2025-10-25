ceb43fc7c1fe12ece48f982c30c80563d19e4bf5
Lukas Truemper <lukas.truemper@outlook.de>
2025-10-25 15:50:04 +0200

    adds if-else test case

--- tests/analysis/control_flow_analysis_test.cpp
+++ tests/analysis/control_flow_analysis_test.cpp
@@ -35,3 +35,39 @@ TEST(ControlFlowAnalysisTest, Sequence) {
     EXPECT_TRUE(analysis.post_dominates(block2, block1));
     EXPECT_FALSE(analysis.post_dominates(block1, block2));
 }
+
+TEST(ControlFlowAnalysisTest, IfElse) {
+    builder::StructuredSDFGBuilder builder("sdfg_1", FunctionType_CPU);
+
+    types::Scalar base_desc(types::PrimitiveType::Int32);
+    builder.add_container("A", base_desc);
+
+    auto& root = builder.subject().root();
+
+    auto& init_block = builder.add_block(root);
+    auto& ifelse = builder.add_if_else(root);
+    auto& then_branch = builder.add_case(ifelse, symbolic::__true__());
+    auto& then_block = builder.add_block(then_branch);
+    auto& else_branch = builder.add_case(ifelse, symbolic::__false__());
+    auto& else_block = builder.add_block(else_branch);
+    auto& join_block = builder.add_block(root);
+
+    // Run analysis
+    analysis::AnalysisManager analysis_manager(builder.subject());
+    auto& analysis = analysis_manager.get<analysis::ControlFlowAnalysis>();
+
+    EXPECT_EQ(analysis.exits().size(), 1);
+    EXPECT_NE(analysis.exits().find(&join_block), analysis.exits().end());
+    EXPECT_TRUE(analysis.dominates(init_block, join_block));
+    EXPECT_TRUE(analysis.dominates(init_block, then_block));
+    EXPECT_TRUE(analysis.dominates(init_block, else_block));
+
+    EXPECT_FALSE(analysis.dominates(then_block, join_block));
+    EXPECT_FALSE(analysis.dominates(else_block, join_block));
+    EXPECT_FALSE(analysis.post_dominates(then_block, init_block));
+    EXPECT_FALSE(analysis.post_dominates(else_block, init_block));
+
+    EXPECT_TRUE(analysis.post_dominates(join_block, init_block));
+    EXPECT_TRUE(analysis.post_dominates(join_block, then_block));
+    EXPECT_TRUE(analysis.post_dominates(join_block, else_block));
+}
