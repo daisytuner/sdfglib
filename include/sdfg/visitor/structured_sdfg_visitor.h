673955f1fdddfc26a229c6616406aebf08da1921
Ramon Wirsch <ramon.wirsch@gmail.com>
2025-11-04 16:41:48 +0100

    Added new SDFG Visitor that stays more true to traditional visitor pattern and can visit a bare SDFG or just parts of it.
     ! still dyn-casting though

--- include/sdfg/visitor/structured_sdfg_visitor.h
+++ include/sdfg/visitor/structured_sdfg_visitor.h
@@ -6,6 +6,12 @@
 namespace sdfg {
 namespace visitor {
 
+/**
+ * Very specific implementation, that technically visits the SDFG of an StructuredSDFGBuilder and not just any SDFG.
+ * Also does some redundant / interleaved calling, where the visitor internally visits a sequence as a node and then
+ * (non-recursively) each child of the sequence and of the child is itself a container, the body/sequence of the
+ * container. This is quite far away from traditional visitor pattern.
+ */
 class StructuredSDFGVisitor {
 protected:
     builder::StructuredSDFGBuilder& builder_;
@@ -50,6 +56,46 @@ public:
     NonStoppingStructuredSDFGVisitor(builder::StructuredSDFGBuilder& builder, analysis::AnalysisManager& analysis_manager);
 
     bool visit() override;
+};
+
+/**
+ * Visitor for Structured SDFG (not a builder).
+ * follows the actual type hierarchy for the visit methods, but still does the hack of dyn-casting instead of an actual
+ * visitor pattern, but this is hidden from users, just less efficient.
+ *
+ */
+class ActualStructuredSDFGVisitor {
+public:
+    ActualStructuredSDFGVisitor();
+
+    virtual ~ActualStructuredSDFGVisitor() = default;
+
+    bool visit(sdfg::structured_control_flow::ControlFlowNode& node);
+
+    bool dispatch(sdfg::structured_control_flow::ControlFlowNode& node); // return value really has no meaning yet.
+                                                                         // Don't know how to do generic results
+                                                                         // efficiently for C++ without making all the
+                                                                         // actual node-types
+
+    virtual bool visit(sdfg::structured_control_flow::Block& node);
+
+    virtual bool visit(sdfg::structured_control_flow::Sequence& node);
+
+    virtual bool visit(sdfg::structured_control_flow::Return& node);
+
+    virtual bool visit(sdfg::structured_control_flow::IfElse& node);
+
+    virtual bool visit(sdfg::structured_control_flow::For& node);
+
+    virtual bool visit(sdfg::structured_control_flow::Map& node);
+
+    virtual bool handleStructuredLoop(sdfg::structured_control_flow::StructuredLoop& loop);
+
+    virtual bool visit(sdfg::structured_control_flow::While& node);
+
+    virtual bool visit(sdfg::structured_control_flow::Continue& node);
+
+    virtual bool visit(sdfg::structured_control_flow::Break& node);
 };
 
 } // namespace visitor
