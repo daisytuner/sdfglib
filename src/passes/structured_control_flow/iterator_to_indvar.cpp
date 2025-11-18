7510ccd6e8c5b0e41a3b40235adc543ce916ce30
Lukas Truemper <lukas.truemper@outlook.de>
2025-11-18 14:44:40 +0100

    adds iterator to indvar pass

---
+++ src/passes/structured_control_flow/iterator_to_indvar.cpp
@@ -0,0 +1,166 @@
+#include "sdfg/passes/structured_control_flow/iterator_to_indvar.h"
+
+#include "sdfg/analysis/assumptions_analysis.h"
+#include "sdfg/analysis/dominance_analysis.h"
+#include "sdfg/analysis/loop_analysis.h"
+#include "sdfg/symbolic/conjunctive_normal_form.h"
+#include "sdfg/symbolic/polynomials.h"
+#include "sdfg/symbolic/series.h"
+
+namespace sdfg {
+namespace passes {
+
+IteratorToIndvar::IteratorToIndvar(builder::StructuredSDFGBuilder& builder, analysis::AnalysisManager& analysis_manager)
+    : NonStoppingStructuredSDFGVisitor(builder, analysis_manager) {
+
+      };
+
+bool IteratorToIndvar::accept(structured_control_flow::For& loop) {
+    // Pattern: recursive pointer update
+    // a = &a[1]
+
+    // Criterion: Find iterator
+    auto& users_analysis = analysis_manager_.get<analysis::Users>();
+    analysis::UsersView body_users(users_analysis, loop.root());
+    if (body_users.moves().size() != 1) {
+        return false;
+    }
+    auto move = body_users.moves().at(0);
+    auto& iterator = move->container();
+
+    // Check whether update is recursive
+    auto& move_dst = static_cast<data_flow::AccessNode&>(*move->element());
+    auto& dfg = move_dst.get_parent();
+    auto& move_edge = *dfg.in_edges(move_dst).begin();
+    if (move_edge.type() != data_flow::MemletType::Reference) {
+        return false;
+    }
+    auto& move_src = static_cast<data_flow::AccessNode&>(move_edge.src());
+    if (move_src.data() != iterator) {
+        return false;
+    }
+
+    // Criterion: Offset must be constant and in bytes
+    auto& move_subset = move_edge.subset();
+    if (move_subset.size() != 1) {
+        return false;
+    }
+    auto offset = move_subset.at(0);
+    if (!SymEngine::is_a<SymEngine::Integer>(*offset)) {
+        return false;
+    }
+    int offset_int = SymEngine::rcp_static_cast<const SymEngine::Integer>(offset)->as_int();
+    if (offset_int <= 0) {
+        return false;
+    }
+    if (move_edge.base_type().type_id() != types::TypeID::Pointer) {
+        return false;
+    }
+    auto& base_type = static_cast<const types::Pointer&>(move_edge.base_type());
+    if (!base_type.has_pointee_type() || base_type.pointee_type().type_id() != types::TypeID::Scalar) {
+        return false;
+    }
+    auto& pointee_type = static_cast<const types::Scalar&>(base_type.pointee_type());
+    if (pointee_type.primitive_type() != types::PrimitiveType::UInt8 &&
+        pointee_type.primitive_type() != types::PrimitiveType::Int8) {
+        return false;
+    }
+
+    // All uses of iterator happen before update
+    std::unordered_set<data_flow::Memlet*> edges;
+    auto& dominance_analysis = analysis_manager_.get<analysis::DominanceAnalysis>();
+    for (auto& use : body_users.uses(iterator)) {
+        // Ignore move
+        if (use == move) {
+            continue;
+        }
+        // Ignore view of move
+        if (use->element() == &move_src && use->use() == analysis::Use::VIEW) {
+            continue;
+        }
+        if (!dynamic_cast<data_flow::AccessNode*>(use->element())) {
+            return false;
+        }
+        auto& use_node = static_cast<data_flow::AccessNode&>(*use->element());
+        auto& use_dfg = use_node.get_parent();
+
+        // No second view
+        if (use->use() == analysis::Use::VIEW) {
+            return false;
+        }
+        // Happens before
+        if (!dominance_analysis.post_dominates(*move, *use)) {
+            return false;
+        }
+
+        if (use->use() == analysis::Use::READ) {
+            for (auto& edge : use_dfg.out_edges(use_node)) {
+                auto& subset = edge.subset();
+                if (subset.size() != 1) {
+                    return false;
+                }
+                if (!symbolic::eq(subset.at(0), symbolic::zero())) {
+                    return false;
+                }
+                // Criterion: offseted bytes should equal interpreted type
+                auto& use_edge_type = edge.base_type();
+                if (use_edge_type.type_id() != types::TypeID::Pointer) {
+                    return false;
+                }
+                auto& use_pointee_type = static_cast<const types::Pointer&>(use_edge_type).pointee_type();
+                if (use_pointee_type.type_id() != types::TypeID::Scalar) {
+                    return false;
+                }
+                auto use_pointee_bitwidth = types::bit_width(use_pointee_type.primitive_type());
+                if (use_pointee_bitwidth != offset_int * 8) {
+                    return false;
+                }
+                edges.insert(&edge);
+            }
+        } else if (use->use() == analysis::Use::WRITE) {
+            for (auto& edge : use_dfg.in_edges(use_node)) {
+                auto& subset = edge.subset();
+                if (subset.size() != 1) {
+                    return false;
+                }
+                if (!symbolic::eq(subset.at(0), symbolic::zero())) {
+                    return false;
+                }
+                // Criterion: offseted bytes should equal interpreted type
+                auto& use_edge_type = edge.base_type();
+                if (use_edge_type.type_id() != types::TypeID::Pointer) {
+                    return false;
+                }
+                auto& use_pointee_type = static_cast<const types::Pointer&>(use_edge_type).pointee_type();
+                if (use_pointee_type.type_id() != types::TypeID::Scalar) {
+                    return false;
+                }
+                auto use_pointee_bitwidth = types::bit_width(use_pointee_type.primitive_type());
+                if (use_pointee_bitwidth != offset_int * 8) {
+                    return false;
+                }
+                edges.insert(&edge);
+            }
+        } else {
+            // Other uses are not allowed
+            return false;
+        }
+    }
+
+    // Step 1: Replace all subsets with iterator - init
+    data_flow::Subset new_subset = {symbolic::sub(loop.indvar(), loop.init())};
+    for (auto& edge : edges) {
+        edge->set_subset(new_subset);
+    }
+
+    // Step 2: Remove iterator
+    auto& block = static_cast<structured_control_flow::Block&>(*dfg.get_parent());
+    builder_.remove_memlet(block, move_edge);
+    builder_.remove_node(block, move_dst);
+    builder_.remove_node(block, move_src);
+
+    return true;
+};
+
+} // namespace passes
+} // namespace sdfg
---
+++ tests/passes/structured_control_flow/iterator_to_indvar_test.cpp
@@ -0,0 +1,55 @@
+#include "sdfg/passes/structured_control_flow/iterator_to_indvar.h"
+
+#include <gtest/gtest.h>
+
+#include <ostream>
+
+#include "sdfg/builder/structured_sdfg_builder.h"
+
+using namespace sdfg;
+
+TEST(IteratorToIndvarTest, ZeroOffsets) {
+    builder::StructuredSDFGBuilder builder("sdfg", FunctionType_CPU);
+
+    types::Pointer opaque_desc;
+    builder.add_container("A", opaque_desc, true);
+
+    types::Scalar sym_desc(types::PrimitiveType::Int32);
+    builder.add_container("i", sym_desc);
+    auto sym = symbolic::symbol("i");
+
+    auto& root = builder.subject().root();
+    auto& loop = builder.add_for(
+        root, sym, symbolic::Lt(sym, symbolic::integer(10)), symbolic::one(), symbolic::add(sym, symbolic::one())
+    );
+
+    // A[0] = A[0] + 1
+    auto& block1 = builder.add_block(loop.root());
+    types::Scalar float_desc(types::PrimitiveType::Float);
+    types::Pointer float_ptr(float_desc);
+    auto& a_input = builder.add_access(block1, "A");
+    auto& one_node = builder.add_constant(block1, "1.0f", float_desc);
+    auto& a_output = builder.add_access(block1, "A");
+
+    auto& tasklet = builder.add_tasklet(block1, data_flow::TaskletCode::fp_add, "_out", {"_in1", "_in2"});
+    auto& edge1 = builder.add_computational_memlet(block1, a_input, tasklet, "_in1", {symbolic::zero()}, float_ptr);
+    auto& edge2 = builder.add_computational_memlet(block1, one_node, tasklet, "_in2", {});
+    auto& edge3 = builder.add_computational_memlet(block1, tasklet, "_out", a_output, {symbolic::zero()}, float_ptr);
+
+    // A = ((int8*) A)[4]
+    auto& block2 = builder.add_block(loop.root());
+    auto& a_iter_in = builder.add_access(block2, "A");
+    auto& a_iter_out = builder.add_access(block2, "A");
+
+    types::Scalar byte_desc(types::PrimitiveType::UInt8);
+    types::Pointer byte_ptr(byte_desc);
+    builder.add_reference_memlet(block2, a_iter_in, a_iter_out, {symbolic::integer(4)}, byte_ptr);
+
+    // Apply pass
+    analysis::AnalysisManager analysis_manager(builder.subject());
+    passes::IteratorToIndvarPass pass;
+    EXPECT_TRUE(pass.run(builder, analysis_manager));
+
+    EXPECT_TRUE(block2.dataflow().nodes().empty());
+    EXPECT_TRUE(block2.dataflow().edges().empty());
+}
