1c37995b3215fd1552490ae979634fb4e4df3f0a
Atrisan <adrianschmitz2@gmail.com>
2025-12-03 11:25:15 +0100

    malloc_usable_size as symengine function

--- src/builder/function_builder.cpp
+++ src/builder/function_builder.cpp
@@ -70,6 +70,7 @@ const types::IType& FunctionBuilder::
     check_name(name);
 
     auto res = this->function().containers_.insert({name, type.clone()});
+    std::cerr << "Added container " << name << " of type " << type.print() << std::endl;
     assert(res.second);
 
     if (is_argument) {
