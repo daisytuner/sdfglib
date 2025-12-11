556ba17d5152aeccde0bdad82d5c43cf110d038d
Lukas Truemper <lukas.truemper@outlook.de>
2025-12-11 21:57:17 +0100

    removes timing outputs of passes

--- src/passes/pass.cpp
+++ src/passes/pass.cpp
@@ -14,17 +14,8 @@ bool Pass::run(builder::SDFGBuilder& builder, bool create_report) {
 };
 
 bool Pass::run(builder::StructuredSDFGBuilder& builder, analysis::AnalysisManager& analysis_manager, bool create_report) {
-    auto start = std::chrono::high_resolution_clock::now();
-    std::cout << "Running pass: " << this->name() << std::endl;
-
     bool applied = this->run_pass(builder, analysis_manager);
     this->invalidates(analysis_manager, applied);
-
-    auto end = std::chrono::high_resolution_clock::now();
-    if (applied) {
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-        std::cout << "Pass " << this->name() << " applied in " << duration << " ms." << std::endl;
-    }
 
 #ifndef NDEBUG
     builder.subject().validate();
