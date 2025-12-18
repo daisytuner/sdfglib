39d2017727a000434e28d14163e27ed3850c637c
Lukas Truemper <lukas.truemper@outlook.de>
2025-12-18 13:52:15 +0100

    improves robustness of type minimization

--- include/sdfg/passes/symbolic/type_minimization.h
+++ include/sdfg/passes/symbolic/type_minimization.h
@@ -14,12 +14,17 @@ namespace sdfg {
 namespace passes {
 
 class TypeMinimization : public visitor::NonStoppingStructuredSDFGVisitor {
+private:
+    bool is_safe_trunc(symbolic::Expression expr, const symbolic::Assumptions& assumptions);
+
 public:
     TypeMinimization(builder::StructuredSDFGBuilder& builder, analysis::AnalysisManager& analysis_manager);
 
     static std::string name() { return "TypeMinimization"; }
 
     virtual bool accept(structured_control_flow::Block& block) override;
+
+    virtual bool accept(structured_control_flow::For& loop) override;
 };
 
 typedef VisitorPass<TypeMinimization> TypeMinimizationPass;
--- src/passes/symbolic/type_minimization.cpp
+++ src/passes/symbolic/type_minimization.cpp
@@ -13,48 +13,53 @@ namespace passes {
 TypeMinimization::TypeMinimization(builder::StructuredSDFGBuilder& builder, analysis::AnalysisManager& analysis_manager)
     : visitor::NonStoppingStructuredSDFGVisitor(builder, analysis_manager) {};
 
+bool TypeMinimization::is_safe_trunc(symbolic::Expression expr, const symbolic::Assumptions& assumptions) {
+    size_t output_bitwidth = 32;
+    int64_t output_min_value_signed = 0;
+    int64_t output_max_value_signed = (1ULL << (output_bitwidth - 1)) - 1;
+
+    auto mini = symbolic::minimum_new(expr, {}, assumptions, true);
+    if (mini.is_null()) {
+        return false;
+    }
+    auto lb_criterion = symbolic::Ge(mini, symbolic::integer(output_min_value_signed));
+    if (!symbolic::is_true(lb_criterion)) {
+        return false;
+    }
+
+    auto maxi = symbolic::maximum_new(expr, {}, assumptions, false);
+    if (maxi.is_null()) {
+        return false;
+    }
+    auto ub_criterion = symbolic::Le(maxi, symbolic::integer(output_max_value_signed));
+    if (!symbolic::is_true(ub_criterion)) {
+        return false;
+    }
+
+    return true;
+}
+
 bool TypeMinimization::accept(structured_control_flow::Block& block) {
     bool applied = false;
     auto& dfg = block.dataflow();
     auto& assumptions_analysis = this->analysis_manager_.get<analysis::AssumptionsAnalysis>();
-    auto& block_assumptions = assumptions_analysis.get(block, false);
+    auto& block_assumptions = assumptions_analysis.get(block, true);
 
     symbolic::ExpressionMap replacements;
     for (auto& edge : dfg.edges()) {
         auto& subset = edge.subset();
         for (auto& dim : subset) {
-            auto truns = symbolic::find<SymEngine::FunctionSymbol>(dim);
-            for (auto& trunc_func : truns) {
-                if (!SymEngine::is_a<symbolic::TruncI32Function>(*trunc_func)) {
+            auto truncs = symbolic::find<SymEngine::FunctionSymbol>(dim);
+            for (auto& trunc : truncs) {
+                auto trunc_func = SymEngine::rcp_static_cast<const SymEngine::FunctionSymbol>(trunc);
+                if (trunc_func->get_name() != "trunc_i32") {
                     continue;
                 }
                 if (replacements.find(trunc_func) != replacements.end()) {
                     continue;
                 }
                 auto arg = trunc_func->get_args()[0];
-
-                size_t output_bitwidth = 32;
-                int64_t output_min_value_signed = 0;
-                int64_t output_max_value_signed = (1ULL << (output_bitwidth - 1)) - 1;
-
-                bool safe_trunc = true;
-                auto mini = symbolic::minimum_new(arg, {}, block_assumptions, true);
-                if (mini.is_null()) {
-                    continue;
-                }
-                auto lb_criterion = symbolic::Ge(mini, symbolic::integer(output_min_value_signed));
-                if (!symbolic::is_true(lb_criterion)) {
-                    replacements[trunc_func] = SymEngine::null;
-                    continue;
-                }
-
-                auto maxi = symbolic::maximum_new(arg, {}, block_assumptions, true);
-                if (maxi.is_null()) {
-                    continue;
-                }
-                auto ub_criterion = symbolic::Le(maxi, symbolic::integer(output_max_value_signed));
-                if (!symbolic::is_true(ub_criterion)) {
-                    replacements[trunc_func] = SymEngine::null;
+                if (!this->is_safe_trunc(arg, block_assumptions)) {
                     continue;
                 }
 
@@ -79,6 +84,45 @@ bool TypeMinimization::accept(structured_control_flow::Block& block) {
         }
         edge.set_subset(subset);
     }
+
+    return applied;
+}
+
+bool TypeMinimization::accept(structured_control_flow::For& loop) {
+    bool applied = false;
+    auto& assumptions_analysis = this->analysis_manager_.get<analysis::AssumptionsAnalysis>();
+    auto& block_assumptions = assumptions_analysis.get(loop, true);
+
+    symbolic::ExpressionMap replacements;
+    auto truncs = symbolic::find<SymEngine::FunctionSymbol>(loop.condition());
+    for (auto& trunc : truncs) {
+        auto trunc_func = SymEngine::rcp_static_cast<const SymEngine::FunctionSymbol>(trunc);
+        if (trunc_func->get_name() != "trunc_i32") {
+            continue;
+        }
+        if (replacements.find(trunc_func) != replacements.end()) {
+            continue;
+        }
+        auto arg = trunc_func->get_args()[0];
+        if (!this->is_safe_trunc(arg, block_assumptions)) {
+            continue;
+        }
+
+        replacements[trunc_func] = arg;
+    }
+    if (replacements.empty()) {
+        return false;
+    }
+
+    auto condition = loop.condition();
+    for (auto& [old_expr, new_expr] : replacements) {
+        if (new_expr.is_null()) {
+            continue;
+        }
+        condition = symbolic::subs(condition, old_expr, new_expr);
+        applied = true;
+    }
+    builder_.update_loop(loop, loop.indvar(), condition, loop.init(), loop.update());
 
     return applied;
 }
