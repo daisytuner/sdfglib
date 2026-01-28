cfa0b39b849bd6c11cfbc624ae957d09fb22b34d
Atrisan <adrianschmitz2@gmail.com>
2026-01-28 13:17:08 +0100

    add heuristic

--- opt/include/sdfg/passes/offloading/cuda_library_node_rewriter_pass.h
+++ opt/include/sdfg/passes/offloading/cuda_library_node_rewriter_pass.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "sdfg/data_flow/library_nodes/math/blas/gemm_node.h"
 #include "sdfg/passes/pass.h"
 #include "sdfg/visitor/structured_sdfg_visitor.h"
 
@@ -12,6 +13,13 @@ public:
 
     static std::string name() { return "CudaLibraryNodeRewriterPass"; };
     bool accept(structured_control_flow::Block& node) override;
+
+private:
+    std::optional<data_flow::ImplementationType>
+    try_library_node_implementation(const data_flow::LibraryNode& lib_node, types::PrimitiveType data_type);
+
+    std::optional<data_flow::ImplementationType>
+    try_cublas_gemm_node_implementation(const math::blas::GEMMNode& gemm_node, types::PrimitiveType data_type);
 };
 
 typedef passes::VisitorPass<CudaLibraryNodeRewriter> CudaLibraryNodeRewriterPass;
