89a6d93e0b05e7462c673a285f70cb68093d20c5
Lukas Truemper <lukas.truemper@outlook.de>
2026-02-05 20:51:40 +0100

    enables ludcmp benchmark in npbench

--- python/benchmarks/npbench/polybench/test_ludcmp.py
+++ python/benchmarks/npbench/polybench/test_ludcmp.py
@@ -39,59 +39,41 @@ def kernel(A, b):
     return x, y
 
 
-@pytest.mark.skip()
 @pytest.mark.parametrize("target", ["none", "sequential", "openmp", "cuda"])
 def test_ludcmp(target):
     if target == "none":
         verifier = SDFGVerification(
             verification={
-                "FOR": 0,
-                "MAP": 0,
-                "SEQUENTIAL": 0,
-                "CUDA": 0,
-                "CPU_PARALLEL": 0,
-                "HIGHWAY": 0,
-                "GEMM": 0,
-                "DOT": 0,
+                "MAP": 2,
+                "SEQUENTIAL": 2,
+                "FOR": 11,
+                "Memset": 2,
+                "Malloc": 2,
             }
         )
     elif target == "sequential":
         verifier = SDFGVerification(
-            verification={
-                "FOR": 0,
-                "MAP": 0,
-                "SEQUENTIAL": 0,
-                "CUDA": 0,
-                "CPU_PARALLEL": 0,
-                "HIGHWAY": 0,
-                "GEMM": 0,
-                "DOT": 0,
-            }
+            verification={"HIGHWAY": 2, "MAP": 2, "FOR": 11, "Memset": 2, "Malloc": 2}
         )
     elif target == "openmp":
         verifier = SDFGVerification(
             verification={
-                "FOR": 0,
-                "MAP": 0,
-                "SEQUENTIAL": 0,
-                "CUDA": 0,
-                "CPU_PARALLEL": 0,
-                "HIGHWAY": 0,
-                "GEMM": 0,
-                "DOT": 0,
+                "CPU_PARALLEL": 2,
+                "MAP": 2,
+                "FOR": 11,
+                "Memset": 2,
+                "Malloc": 2,
             }
         )
     else:  # cuda
         verifier = SDFGVerification(
             verification={
-                "FOR": 0,
-                "MAP": 0,
-                "SEQUENTIAL": 0,
-                "CUDA": 0,
-                "CPU_PARALLEL": 0,
-                "HIGHWAY": 0,
-                "GEMM": 0,
-                "DOT": 0,
+                "CUDA": 2,
+                "MAP": 2,
+                "CUDAOffloading": 8,
+                "FOR": 11,
+                "Memset": 2,
+                "Malloc": 2,
             }
         )
     run_pytest(initialize, kernel, PARAMETERS, target, verifier=verifier)
